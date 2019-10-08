d4036f9f414a5d8a4643cfd7542da1fc2d51c442
Ryan Dwyer <ryandwyer1@gmail.com>
2019-10-08 22:26:43 +1000

    Decompile ai004a

--- src/game/chr/chraicommands.c
+++ src/game/chr/chraicommands.c
@@ -1943,37 +1943,17 @@ bool ai0049(void)
 /**
  * @cmd 004a
  */
-GLOBAL_ASM(
-glabel ai004a
-/*  f050464:	27bdffe8 */ 	addiu	$sp,$sp,-24
-/*  f050468:	3c03800a */ 	lui	$v1,0x800a
-/*  f05046c:	24639fc0 */ 	addiu	$v1,$v1,-24640
-/*  f050470:	afbf0014 */ 	sw	$ra,0x14($sp)
-/*  f050474:	0fc127f3 */ 	jal	func0f049fcc
-/*  f050478:	8c640424 */ 	lw	$a0,0x424($v1)
-/*  f05047c:	3c03800a */ 	lui	$v1,0x800a
-/*  f050480:	1040000a */ 	beqz	$v0,.L0f0504ac
-/*  f050484:	24639fc0 */ 	addiu	$v1,$v1,-24640
-/*  f050488:	8c640434 */ 	lw	$a0,0x434($v1)
-/*  f05048c:	8c650438 */ 	lw	$a1,0x438($v1)
-/*  f050490:	00851021 */ 	addu	$v0,$a0,$a1
-/*  f050494:	0fc13583 */ 	jal	chraiGoToLabel
-/*  f050498:	90460002 */ 	lbu	$a2,0x2($v0)
-/*  f05049c:	3c03800a */ 	lui	$v1,0x800a
-/*  f0504a0:	24639fc0 */ 	addiu	$v1,$v1,-24640
-/*  f0504a4:	10000004 */ 	beqz	$zero,.L0f0504b8
-/*  f0504a8:	ac620438 */ 	sw	$v0,0x438($v1)
-.L0f0504ac:
-/*  f0504ac:	8c6e0438 */ 	lw	$t6,0x438($v1)
-/*  f0504b0:	25cf0003 */ 	addiu	$t7,$t6,0x3
-/*  f0504b4:	ac6f0438 */ 	sw	$t7,0x438($v1)
-.L0f0504b8:
-/*  f0504b8:	8fbf0014 */ 	lw	$ra,0x14($sp)
-/*  f0504bc:	27bd0018 */ 	addiu	$sp,$sp,0x18
-/*  f0504c0:	00001025 */ 	or	$v0,$zero,$zero
-/*  f0504c4:	03e00008 */ 	jr	$ra
-/*  f0504c8:	00000000 */ 	sll	$zero,$zero,0x0
-);
+bool ai004a(void)
+{
+	if (func0f049fcc(g_Vars.chrdata)) {
+		u8 *cmd = g_Vars.ailist + g_Vars.aioffset;
+		g_Vars.aioffset = chraiGoToLabel(g_Vars.ailist, g_Vars.aioffset, cmd[2]);
+	} else {
+		g_Vars.aioffset += 3;
+	}
+
+	return false;
+}
 
 /**
  * @cmd 004b
