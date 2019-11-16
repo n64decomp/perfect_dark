d0ae6b77e9120b1ff12163ce7fc31b10f6724c28
Ryan Dwyer <ryandwyer1@gmail.com>
2019-11-16 13:26:12 +1000

    Decompile func0f176000, func0f176008, func0f17601c, func0f176030, func0f176044, func0f176058, func0f176060 and func0f176074

--- src/game/game_176000.c
+++ src/game/game_176000.c
@@ -9,42 +9,42 @@
 #include "game/game_175f50.h"
 #include "library/library.h"
 
-GLOBAL_ASM(
-glabel func0f176000
-/*  f176000:	03e00008 */ 	jr	$ra
-/*  f176004:	00801025 */ 	or	$v0,$a0,$zero
-);
+s32 func0f176000(s32 arg0)
+{
+	return arg0;
+}
 
-GLOBAL_ASM(
-glabel func0f176008
-/*  f176008:	afa50004 */ 	sw	$a1,0x4($sp)
-/*  f17600c:	afa60008 */ 	sw	$a2,0x8($sp)
-/*  f176010:	afa7000c */ 	sw	$a3,0xc($sp)
-/*  f176014:	03e00008 */ 	jr	$ra
-/*  f176018:	00801025 */ 	or	$v0,$a0,$zero
-/*  f17601c:	afa50004 */ 	sw	$a1,0x4($sp)
-/*  f176020:	afa60008 */ 	sw	$a2,0x8($sp)
-/*  f176024:	afa7000c */ 	sw	$a3,0xc($sp)
-/*  f176028:	03e00008 */ 	jr	$ra
-/*  f17602c:	00801025 */ 	or	$v0,$a0,$zero
-/*  f176030:	afa50004 */ 	sw	$a1,0x4($sp)
-/*  f176034:	afa60008 */ 	sw	$a2,0x8($sp)
-/*  f176038:	afa7000c */ 	sw	$a3,0xc($sp)
-/*  f17603c:	03e00008 */ 	jr	$ra
-/*  f176040:	00801025 */ 	or	$v0,$a0,$zero
-/*  f176044:	afa50004 */ 	sw	$a1,0x4($sp)
-/*  f176048:	afa60008 */ 	sw	$a2,0x8($sp)
-/*  f17604c:	afa7000c */ 	sw	$a3,0xc($sp)
-/*  f176050:	03e00008 */ 	jr	$ra
-/*  f176054:	00801025 */ 	or	$v0,$a0,$zero
-/*  f176058:	03e00008 */ 	jr	$ra
-/*  f17605c:	e7ac0000 */ 	swc1	$f12,0x0($sp)
-/*  f176060:	afa50004 */ 	sw	$a1,0x4($sp)
-/*  f176064:	afa60008 */ 	sw	$a2,0x8($sp)
-/*  f176068:	afa7000c */ 	sw	$a3,0xc($sp)
-/*  f17606c:	03e00008 */ 	jr	$ra
-/*  f176070:	00801025 */ 	or	$v0,$a0,$zero
-/*  f176074:	03e00008 */ 	jr	$ra
-/*  f176078:	00000000 */ 	sll	$zero,$zero,0x0
-/*  f17607c:	00000000 */ 	sll	$zero,$zero,0x0
-);
+s32 func0f176008(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+	return arg0;
+}
+
+s32 func0f17601c(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+	return arg0;
+}
+
+s32 func0f176030(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+	return arg0;
+}
+
+s32 func0f176044(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+	return arg0;
+}
+
+void func0f176058(float arg0)
+{
+	// empty
+}
+
+s32 func0f176060(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+	return arg0;
+}
+
+void func0f176074(void)
+{
+	// empty
+}
