61403b1e749abfccf4a0696d5de77919678f9cc8
Ryan Dwyer <ryandwyer1@gmail.com>
2020-02-16 18:03:03 +1000

    Decompile func0f1a7878

--- src/game/game_1a3340.c
+++ src/game/game_1a3340.c
@@ -4889,20 +4889,7 @@ void *fileLoad(u16 fileid)
 	return func0f1a7794(fileid, 0, 0, 0);
 }
 
-GLOBAL_ASM(
-glabel func0f1a7878
-/*  f1a7878:	27bdffe8 */ 	addiu	$sp,$sp,-24
-/*  f1a787c:	afbf0014 */ 	sw	$ra,0x14($sp)
-/*  f1a7880:	afa40018 */ 	sw	$a0,0x18($sp)
-/*  f1a7884:	308effff */ 	andi	$t6,$a0,0xffff
-/*  f1a7888:	01c02025 */ 	or	$a0,$t6,$zero
-/*  f1a788c:	0fc69de5 */ 	jal	func0f1a7794
-/*  f1a7890:	00003825 */ 	or	$a3,$zero,$zero
-/*  f1a7894:	8fbf0014 */ 	lw	$ra,0x14($sp)
-/*  f1a7898:	27bd0018 */ 	addiu	$sp,$sp,0x18
-/*  f1a789c:	03e00008 */ 	jr	$ra
-/*  f1a78a0:	00000000 */ 	sll	$zero,$zero,0x0
-/*  f1a78a4:	00000000 */ 	sll	$zero,$zero,0x0
-/*  f1a78a8:	00000000 */ 	sll	$zero,$zero,0x0
-/*  f1a78ac:	00000000 */ 	sll	$zero,$zero,0x0
-);
+void *func0f1a7878(u16 fileid, s32 arg1, s32 arg2)
+{
+	return func0f1a7794(fileid, arg1, arg2, 0);
+}
