bfb8346150fb59f49a51a794eb7bde8d9a8c18b3
Ryan Dwyer <ryandwyer1@gmail.com>
2020-03-08 00:09:24 +1000

    Split game_0dcdb0 into game_0e0770

--- src/include/game/game_0dcdb0.h
+++ src/include/game/game_0dcdb0.h
@@ -26,28 +26,5 @@ void currentPlayerSetFlag(u32 flag);
 void func0f0dfa50(u32 playernum);
 u32 func0f0dfad0(void);
 u32 func0f0e0724(void);
-u32 func0f0e0770(void);
-u32 func0f0e0998(void);
-u32 func0f0e0dac(void);
-u32 func0f0e1668(void);
-u32 func0f0e194c(void);
-u32 func0f0e1ce8(void);
-u32 func0f0e1fac(void);
-u32 func0f0e2348(void);
-u32 func0f0e2498(void);
-u32 func0f0e258c(void);
-u32 func0f0e26ec(void);
-u32 func0f0e2744(void);
-u32 func0f0e2aa4(void);
-u32 func0f0e2ee8(void);
-u32 func0f0e2f5c(void);
-u32 func0f0e2fe4(void);
-u32 func0f0e3324(void);
-u32 func0f0e37b0(void);
-u32 func0f0e39d0(void);
-u32 func0f0e4190(void);
-u32 func0f0e4fd4(void);
-u32 func0f0e4fe0(void);
-u32 func0f0e5000(void);
 
 #endif
---
+++ src/include/game/game_0e0770.h
@@ -0,0 +1,30 @@
+#ifndef IN_GAME_GAME_0E0770_H
+#define IN_GAME_GAME_0E0770_H
+#include <ultra64.h>
+#include "types.h"
+
+u32 func0f0e0770(void);
+u32 func0f0e0998(void);
+u32 func0f0e0dac(void);
+u32 func0f0e1668(void);
+u32 func0f0e194c(void);
+u32 func0f0e1ce8(void);
+u32 func0f0e1fac(void);
+u32 func0f0e2348(void);
+u32 func0f0e2498(void);
+u32 func0f0e258c(void);
+u32 func0f0e26ec(void);
+u32 func0f0e2744(void);
+u32 func0f0e2aa4(void);
+u32 func0f0e2ee8(void);
+u32 func0f0e2f5c(void);
+u32 func0f0e2fe4(void);
+u32 func0f0e3324(void);
+u32 func0f0e37b0(void);
+u32 func0f0e39d0(void);
+u32 func0f0e4190(void);
+u32 func0f0e4fd4(void);
+u32 func0f0e4fe0(void);
+u32 func0f0e5000(void);
+
+#endif
