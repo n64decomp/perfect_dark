07839f3744ece311bc3c27337e593ecfca4bb80d
Ryan Dwyer <ryandwyer1@gmail.com>
2020-03-29 14:08:51 +1000

    Fix player->unk1580 bitfield

--- src/game/game_0fd660.c
+++ src/game/game_0fd660.c
@@ -1200,7 +1200,7 @@ glabel func0f0fecd4
 
 void activemenuOpen(void)
 {
-	if ((g_Vars.currentplayer->unk1580_s32 & 1) == 0) {
+	if (g_Vars.currentplayer->unk1583_07 == 0) {
 		g_ActiveMenuIndex = g_Vars.currentplayernum;
 		g_Vars.currentplayer->activemenumode = 1;
 		g_PlayersWithControl[g_Vars.currentplayernum] = false;
