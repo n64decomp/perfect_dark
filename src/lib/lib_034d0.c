0a345cc5d621e1946719aeb10d5e9af0e6c81a29
Ryan Dwyer <ryandwyer1@gmail.com>
2020-11-29 10:13:48 +1000

    Decompile func000034e0

--- src/lib/lib_034d0.c
+++ src/lib/lib_034d0.c
@@ -10,10 +10,7 @@ void func000034d8(void)
 	// empty
 }
 
-GLOBAL_ASM(
-glabel func000034e0
-/*     34e0:	03e00008 */ 	jr	$ra
-/*     34e4:	afa40000 */ 	sw	$a0,0x0($sp)
-/*     34e8:	00000000 */ 	nop
-/*     34ec:	00000000 */ 	nop
-);
+Gfx *func000034e0(Gfx *gdl)
+{
+	// empty
+}
