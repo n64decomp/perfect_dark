ec28172ec67fc4d100b0999ad4b8ebe3cc4bec58
Ryan Dwyer <ryandwyer1@gmail.com>
2021-01-29 23:37:23 +1000

    Decompile __osViGetCurrentContext

--- src/lib/ultra/io/vigetcurrcontext.c
+++ src/lib/ultra/io/vigetcurrcontext.c
@@ -1,9 +1,7 @@
 #include <ultra64.h>
+#include "game/data/data_000000.h"
 
-GLOBAL_ASM(
-glabel __osViGetCurrentContext
-/*    49780:	3c028006 */ 	lui	$v0,%hi(__osViCurr)
-/*    49784:	03e00008 */ 	jr	$ra
-/*    49788:	8c420910 */ 	lw	$v0,%lo(__osViCurr)($v0)
-/*    4978c:	00000000 */ 	nop
-);
+__OSViContext *__osViGetCurrentContext(void)
+{
+	return __osViCurr;
+}
