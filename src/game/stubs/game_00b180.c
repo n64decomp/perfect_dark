f63a2fb6d126f5a79165bd139d5d7b9bc57edde9
Ryan Dwyer <ryandwyer1@gmail.com>
2021-03-05 00:00:55 +1000

    Decompile stub0f00b180

--- src/game/stubs/game_00b180.c
+++ src/game/stubs/game_00b180.c
@@ -1,9 +1,6 @@
 #include <ultra64.h>
 
-GLOBAL_ASM(
-glabel stub0f00b180
-/*  f00b180:	03e00008 */ 	jr	$ra
-/*  f00b184:	00000000 */ 	nop
-/*  f00b188:	00000000 */ 	nop
-/*  f00b18c:	00000000 */ 	nop
-);
+void stub0f00b180(void)
+{
+	// empty
+}
