e31a2b2292b77df0366e8443d3631b2bff70d939
Ryan Dwyer <ryandwyer1@gmail.com>
2021-10-12 19:39:10 +1000

    Decompile func00038d18

--- src/lib/lib_38d10.c
+++ src/lib/lib_38d10.c
@@ -8,9 +8,12 @@ GLOBAL_ASM(
 glabel func00038d10
 /*    38d10:	03e00008 */ 	jr	$ra
 /*    38d14:	8c82002c */ 	lw	$v0,0x2c($a0)
-/*    38d18:	03e00008 */ 	jr	$ra
-/*    38d1c:	00000000 */ 	nop
 );
+
+void func00038d18(void)
+{
+	// empty
+}
 
 void func00038d20(void)
 {
