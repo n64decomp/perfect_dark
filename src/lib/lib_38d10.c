6f0b1baa6db19f31e608ea859138a4859bba72e8
Ryan Dwyer <ryandwyer1@gmail.com>
2021-10-12 19:38:52 +1000

    Decompile func00038d20

--- src/lib/lib_38d10.c
+++ src/lib/lib_38d10.c
@@ -10,6 +10,9 @@ glabel func00038d10
 /*    38d14:	8c82002c */ 	lw	$v0,0x2c($a0)
 /*    38d18:	03e00008 */ 	jr	$ra
 /*    38d1c:	00000000 */ 	nop
-/*    38d20:	03e00008 */ 	jr	$ra
-/*    38d24:	00000000 */ 	nop
 );
+
+void func00038d20(void)
+{
+	// empty
+}
