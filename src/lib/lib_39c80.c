881a3a7047f5ba98fbf37f166d7e842b882f2470
Ryan Dwyer <ryandwyer1@gmail.com>
2021-10-31 23:27:55 +1000

    Decompile func00039ecc

--- src/lib/lib_39c80.c
+++ src/lib/lib_39c80.c
@@ -66,27 +66,12 @@ glabel func00039e5c
 /*    39ec0:	27bd0020 */ 	addiu	$sp,$sp,0x20
 /*    39ec4:	03e00008 */ 	jr	$ra
 /*    39ec8:	00000000 */ 	nop
-/*    39ecc:	27bdffe0 */ 	addiu	$sp,$sp,-32
-/*    39ed0:	afbf001c */ 	sw	$ra,0x1c($sp)
-/*    39ed4:	afa40020 */ 	sw	$a0,0x20($sp)
-/*    39ed8:	afa50024 */ 	sw	$a1,0x24($sp)
-/*    39edc:	afa60028 */ 	sw	$a2,0x28($sp)
-/*    39ee0:	8fa60024 */ 	lw	$a2,0x24($sp)
-/*    39ee4:	93ae002b */ 	lbu	$t6,0x2b($sp)
-/*    39ee8:	8fa40020 */ 	lw	$a0,0x20($sp)
-/*    39eec:	00002825 */ 	or	$a1,$zero,$zero
-/*    39ef0:	24070041 */ 	addiu	$a3,$zero,0x41
-/*    39ef4:	34c600b0 */ 	ori	$a2,$a2,0xb0
-/*    39ef8:	0c011194 */ 	jal	n_alCSPSendMidi
-/*    39efc:	afae0010 */ 	sw	$t6,0x10($sp)
-/*    39f00:	10000001 */ 	b	.L00039f08
-/*    39f04:	00000000 */ 	nop
-.L00039f08:
-/*    39f08:	8fbf001c */ 	lw	$ra,0x1c($sp)
-/*    39f0c:	27bd0020 */ 	addiu	$sp,$sp,0x20
-/*    39f10:	03e00008 */ 	jr	$ra
-/*    39f14:	00000000 */ 	nop
 );
+
+void func00039ecc(N_ALCSPlayer *seqp, u32 arg1, u8 arg2)
+{
+	n_alCSPSendMidi(seqp, 0, arg1 | 0xb0, 0x41, arg2);
+}
 
 void func00039f18(N_ALCSPlayer *seqp, u32 arg1, u8 arg2)
 {
