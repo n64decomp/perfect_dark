87dc56469e9f4c4d7b4f88b8bdb986059dddf06d
Ryan Dwyer <ryandwyer1@gmail.com>
2021-11-06 13:40:14 +1000

    Decompile mtx00017588

--- src/include/lib/mtx.h
+++ src/include/lib/mtx.h
@@ -58,7 +58,7 @@ void mtx4LoadRotationFrom(f32 src[4][4], f32 dst[4][4]);
 u32 mtx000170e4(void);
 u32 mtx0001719c(void);
 u32 mtx000172f0(void);
-u32 mtx00017588(void);
+void mtx00017588(f32 arg0[4][4], f32 arg1[4][4]);
 void mtx00017614(f32 arg0[4][4], f32 arg1[4][4]);
 f32 mtx00017a78(f32 arg0[4][4]);
 f32 mtx00017c2c(f32 arg0, f32 arg1, f32 arg2, f32 arg3, f32 arg4, f32 arg5, f32 arg6, f32 arg7, f32 arg8);
