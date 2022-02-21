34197b1907173474d34ff26dea14edabfd987685
Ryan Dwyer <ryandwyer1@gmail.com>
2022-02-21 18:53:46 +1000

    Decompile quaternion0f097518

--- src/include/game/game_096ca0.h
+++ src/include/game/game_096ca0.h
@@ -9,7 +9,7 @@ void quaternionToMtx(f32 *arg0, Mtxf *arg1);
 void quaternion0f097044(Mtxf *matrix, f32 arg1[4]);
 void quaternionToTransformMtx(struct coord *pos, f32 rot[4], Mtxf *matrix);
 void quaternionSlerp(f32 q1[4], f32 q2[4], f32 t, f32 result[4]);
-u32 func0f097518(void);
+void quaternion0f097518(f32 q[4], f32 t, f32 result[4]);
 void quaternion0f0976c0(f32 q1[4], f32 q2[4]);
 void quaternionMultQuaternion(f32 a[4], f32 b[4], f32 result[4]);
 
