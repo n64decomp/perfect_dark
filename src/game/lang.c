ba4cf156cee9624c28ca67dda9d0917d77bbb07e
Ryan Dwyer <ryandwyer1@gmail.com>
2022-03-20 16:04:42 +1000

    pal-beta, pal-final and jpn-final: Decompile langReset

--- src/game/lang.c
+++ src/game/lang.c
@@ -8,14 +8,50 @@
 #include "types.h"
 
 /**
- * NTSC appears to use a boolean for Japanese support.
- * PAL ignores this mostly and introduces g_LanguageId.
+ * Officially, the NTSC versions are American English only, while the PAL
+ * versions support British English, French, German, Italian and Spanish, and
+ * the Japanese version is Japanese only. However all versions actually have
+ * support for all 7 languages, despite some being inaccessible and not even
+ * translated.
+ *
+ * All strings are categorised into areas, which we call banks. There is one
+ * bank per stage, as well as some common banks such as menus and weapon names.
+ * Each bank contains 7 files; one for each translation.
+ *
+ * An example language filename is LoptionsE. "options" is the bank name, and E
+ * means it's the American English file.
+ *
+ * The list of suffixes are:
+ * E - American English
+ * J - Japanese
+ * P - British English (P is for PAL)
+ * _str_f = French
+ * _str_g = German
+ * _str_i = Italian
+ * _str_s = Spanish
+ *
+ * Banks are loaded based on the current stage. When loading a bank, only the
+ * current translation is loaded.
+ *
+ * Text is referenced using a 16 bit text ID. The text ID is made up of a 7-bit
+ * bank index and a 9-bit string index within that bank:
+ *
+ *     bbbbbbbs ssssssss
+ *
+ * For versions prior to PAL, the language files are loaded during stage load
+ * and allocated from memp. This method doesn't work for PAL because PAL
+ * supports changing the language mid-stage and memp allocations cannot be freed
+ * until a new stage is loaded. So for PAL, during stage load a single buffer is
+ * allocated from memp to hold all loaded language files. The buffer is
+ * repopulated when the language is changed, and the lang banks point to their
+ * relevant offsets within that buffer.
  */
+
 #if VERSION >= VERSION_PAL_BETA
 bool g_Jpn = VERSION == VERSION_JPN_FINAL ? true : false;
-s32 var80084664pf = 0;
-s32 var80084668pf = 0;
-s32 var8008466cpf = 0;
+u8 *g_LangBuffer = NULL;
+u8 *g_LangBufferPos = NULL;
+s32 g_LangBufferSize = 0;
 u32 *g_LangBanks[69];
 void *var800aabb4;
 struct var800aabb8 *var800aabb8;
@@ -652,11 +688,11 @@ void langLoad(s32 bank)
 #if VERSION >= VERSION_PAL_BETA
 	s32 len = fileGetInflatedSize(langGetFileId(bank));
 
-	if (var80084664pf + len + var8008466cpf - var80084668pf >= 0) {
-		s32 len2 = var80084664pf + var8008466cpf - var80084668pf;
+	if ((s32)g_LangBuffer + len + g_LangBufferSize - (s32)g_LangBufferPos >= 0) {
+		s32 len2 = (s32)g_LangBuffer + g_LangBufferSize - (s32)g_LangBufferPos;
 		len2 = len2 / 32 * 32;
-		g_LangBanks[bank] = fileLoadToAddr(langGetFileId(bank), FILELOADMETHOD_DEFAULT, (u8 *)var80084668pf, len2);
-		var80084668pf = align32(var80084668pf + len);
+		g_LangBanks[bank] = fileLoadToAddr(langGetFileId(bank), FILELOADMETHOD_DEFAULT, (u8 *)g_LangBufferPos, len2);
+		g_LangBufferPos = (u8 *)(align32((s32)g_LangBufferPos + len));
 	} else {
 		CRASH();
 	}
@@ -708,7 +744,7 @@ void langReload(void)
 {
 	s32 i;
 
-	var80084668pf = align32(var80084664pf);
+	g_LangBufferPos = (u8 *)align32(g_LangBuffer);
 
 	for (i = 0; i < ARRAYCOUNT(g_LangBanks); i++) {
 		if (g_LangBanks[i] != NULL) {
