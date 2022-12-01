d6123cbe724a2dbdec11ecdaa1713110cd2599b8
Ryan Dwyer <ryandwyer1@gmail.com>
2022-12-01 19:09:56 +1000

    Remove apparent recursion from strcmp and strncmp

--- src/lib/str.c
+++ src/lib/str.c
@@ -80,42 +80,48 @@ char *strcat(char *dst, const char *src)
 
 s32 strcmp(const char *s1, const char *s2)
 {
-	if (*s1 != *s2) {
-		if (*s1 < *s2) {
-			return -1;
-		} else {
-			return 1;
+	while (true) {
+		if (*s1 != *s2) {
+			if (*s1 < *s2) {
+				return -1;
+			} else {
+				return 1;
+			}
 		}
-	}
 
-	if (*s1 == '\0') {
-		return 0;
-	}
+		if (*s1 == '\0') {
+			return 0;
+		}
 
-	return strcmp(s1 + 1, s2 + 1);
+		s1++;
+		s2++;
+	}
 }
 
 s32 strncmp(const char *s1, const char *s2, s32 len)
 {
-	if (len == 0) {
-		return 0;
-	}
-
-	len--;
-
-	if (*s1 != *s2) {
-		if (*s1 < *s2) {
-			return -1;
-		} else {
-			return 1;
+	while (true) {
+		if (len == 0) {
+			return 0;
 		}
-	}
 
-	if (*s1 == '\0') {
-		return 0;
-	}
+		if (*s1 != *s2) {
+			if (*s1 < *s2) {
+				return -1;
+			} else {
+				return 1;
+			}
+		}
 
-	return strncmp(s1 + 1, s2 + 1, len);
+		if (*s1 == '\0') {
+			return 0;
+		}
+
+		len--;
+
+		s1++;
+		s2++;
+	}
 }
 
 char toupper(char c)
